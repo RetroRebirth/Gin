abbd0da975003a3b7d6a7cc04c8539e452f787ad
Christopher Williams <chrsw49@gmail.com>
2017-10-06 01:08:02 -0700

    added clear functionality & undo states

--- Gin/ViewController.swift
+++ Gin/ViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct IntStack {
+        var items = [Int]()
+        mutating func push(item: Int) {
+            items.append(item)
+        }
+        mutating func pop() -> Int {
+            return items.removeLast()
+        }
+        func isEmpty() -> Bool {
+            return items.isEmpty
+        }
+        mutating func clear() {
+            items.removeAll()
+        }
+    }
 
     @IBOutlet weak var p1First: UILabel!
     @IBOutlet weak var p1Second: UILabel!
     @IBOutlet weak var p1Third: UILabel!
     
+    @IBOutlet weak var undoButton: UIButton!
+    
     @IBOutlet weak var calcOverlay: UIView!
     @IBOutlet weak var calcDisplay: UILabel!
     
     let numFormat: String = "%03d"
+    
+    var undos: IntStack = IntStack()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +50,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func p1AddTapped(sender: AnyObject) {
-        calcDisplay.text = String(format: numFormat, 0)
+        calcDisplay.text = String(format: numFormat, 000)
         calcOverlay.hidden = false
     }
 
     @IBAction func p1UndoTapped(sender: AnyObject) {
-        p1First.text = String(format: numFormat, Int(p1First.text!)! - Int(calcDisplay.text!)!)
+        let num = undos.pop()
+        p1First.text = String(format: numFormat, Int(p1First.text!)! - num)
+        
+        if undos.isEmpty() {
+            undoButton.enabled = false
+        }
+    }
+    
+    @IBAction func p1ClearTapped(sender: AnyObject) {
+        p1First.text = String(format: numFormat, 000)
+        undos.clear()
+        
+        undoButton.enabled = false
     }
     
     @IBAction func calc1Tapped(sender: AnyObject) { calcNumTapped(1) }
@@ -48,14 +80,20 @@ class ViewController: UIViewController {
     @IBAction func calc8Tapped(sender: AnyObject) { calcNumTapped(8) }
     @IBAction func calc9Tapped(sender: AnyObject) { calcNumTapped(9) }
     @IBAction func calc0Tapped(sender: AnyObject) { calcNumTapped(0) }
-
     func calcNumTapped(num: Int) {
         calcDisplay.text = String(format: numFormat, Int(calcDisplay.text!)!*10 + num)
     }
     
     @IBAction func calcSubmitTapped(sender: AnyObject) {
         calcOverlay.hidden = true
-        p1First.text = String(format: numFormat, Int(p1First.text!)! + Int(calcDisplay.text!)!)
+        
+        let num = Int(calcDisplay.text!)!
+        p1First.text = String(format: numFormat, Int(p1First.text!)! + num)
+        undos.push(num)
+        
+        if !undoButton.enabled {
+            undoButton.enabled = true
+        }
     }
 }
 
